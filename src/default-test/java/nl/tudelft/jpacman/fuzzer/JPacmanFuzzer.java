827c3533fb8a00527e1ec08ad54170a71baee20c
Casper Boone <mail@casperboone.nl>
2019-04-20 20:00:33 +0200

    Refactor fuzzer
    It now uses @RepeatedTest for test repetition. Other refactor steps are mostly about code style and consistency.

--- src/default-test/java/nl/tudelft/jpacman/fuzzer/JPacmanFuzzer.java
+++ src/default-test/java/nl/tudelft/jpacman/fuzzer/JPacmanFuzzer.java
@@ -1,159 +1,128 @@
 package nl.tudelft.jpacman.fuzzer;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import nl.tudelft.jpacman.Launcher;
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.level.Player;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import java.io.OutputStreamWriter;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream; 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.io.File;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * A basic fuzzer trying out random moves
- * and writing logs in output files.  
- * Number of runs is a tunable parameter.
- * Each run initiates the game and is played 
- * until pacman dies.
+ * A basic fuzzer trying out random moves and writing logs in output files. Number of runs is a
+ * tunable parameter. Each run initiates the game and is played until PacMan dies.
  *
- * @author Azqa Nadeem, April 2019.
+ * @author Azqa Nadeem
  */
-public class JPacmanFuzzer {
+class JPacmanFuzzer {
+    private static final int RUNS = 5;
+    private static final String LOG_HEADER =
+        "isAlive, hasCollided, currentDirection, nextDirection, remainingPellets, score";
+
     private Launcher launcher;
-    private BufferedWriter writer;
-    @SuppressWarnings("magicnumber")
-    private static int numRuns = 5;
+    private BufferedWriter logWriter;
 
     /**
      * Sets and cleans up directory for storing logs.
+     *
      * @throws IOException when file/folder can't be created.
      */
-    void setUpLogging() throws IOException {
+    @BeforeAll
+    static void setUpLogging() throws IOException {
         File dir = new File("logs");
-        if (dir != null) {
-            if (!dir.exists()) {
-                if (!dir.mkdirs()) {
-                    throw new IOException("Problem when creating logs/");
-                }
-            }
-            File[] files = dir.listFiles();
-            if (files != null) {
-                for (File f: files) { 
-                    if (!f.isDirectory()) {
-                        if (!f.delete()) {
-                            throw new IOException("Problem when deleting logs");
-                        }
-                    } 
-                }
-            } else {
-                throw new IOException("Problem when deleting logs");
-            }
+        if (!dir.exists() && !dir.mkdirs()) {
+            throw new IOException("Directory `logs/` could not be created.");
         }
+        Arrays.stream(dir.listFiles()).forEach(File::delete);
     }
+
     /**
      * Launch the user interface.
      */
-    void setUpPacman() {
+    @BeforeEach
+    void setUpGame() {
         launcher = new Launcher();
         launcher.launch();
     }
 
     /**
      * Quit the user interface when we're done.
+     *
+     * @throws IOException when log writer cannot be closed.
      */
     @AfterEach
-    void tearDown() {
+    void tearDown() throws IOException {
         launcher.dispose();
-        try {
-            writer.flush();
-            writer.close();
-        } catch (IOException e) { 
-        }
-        
+        logWriter.flush();
+        logWriter.close();
     }
 
     /**
-     * 
      * Basic fuzzer implementation.
-     * 
+     *
+     * @param repetitionInfo repeated test information
+     * @throws IOException when the log write created.
      */
-    @SuppressWarnings({"magicnumber", "methodlength", "PMD.JUnitTestContainsTooManyAsserts"})
-    @Test
-    void fuzzerTest() {
-        int fnum = 0;
+    @RepeatedTest(RUNS)
+    void fuzzerTest(RepetitionInfo repetitionInfo) throws IOException {
+        Game game = launcher.getGame();
+        Player player = game.getPlayers().get(0);
+        Direction chosen = Direction.EAST;
+
+        String filename = "logs/log" + repetitionInfo.getCurrentRepetition() + ".txt";
+        logWriter = new BufferedWriter(new OutputStreamWriter(
+            new FileOutputStream(filename, true), StandardCharsets.UTF_8));
+        logWriter.write(LOG_HEADER);
 
         try {
-            setUpLogging();
-        } catch (IOException e) {
-        }
-        
+            game.start();
+            assertThat(game.isInProgress()).isTrue();
 
-        for (int i = 0; i < numRuns; i++) {
-            fnum++;
-            setUpPacman();
-            Game game = launcher.getGame();
-            Player player = game.getPlayers().get(0);
-            String filename = "logs/log" + fnum + ".txt";
-            Direction chosen = Direction.EAST; 
+            while (game.isInProgress()) {
+                chosen = getRandomDirection();
 
- 
-            try {
-                // start cleanly. 
-                assertThat(game.isInProgress()).isFalse();
-                game.start();
-
-                writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(filename, true), StandardCharsets.UTF_8));
-                writer.write("isAlive, hasCollided, currentDirection, "
-                    + "nextDirection, remainingPellets, score");
-
-                while (game.isInProgress()) {
-                    double rand = Math.random();
-                    if (rand <= 0.25) {
-                        chosen = Direction.NORTH;
-                    } else if (rand > 0.25 && rand <= 0.5) {
-                        chosen = Direction.EAST;
-                    } else if (rand > 0.5 && rand <= 0.75) {
-                        chosen = Direction.SOUTH;
-                    } else {
-                        chosen = Direction.WEST;
-                    }
-                    log(game, player, chosen, writer);
-                    game.getLevel().move(player, chosen);
-                }
-                log(game, player, chosen, writer);
-                game.stop();
-                tearDown();
-            } catch (IOException e) {
-            } catch (RuntimeException e) {
-                log(game, player, chosen, writer);
-                game.stop();
-                tearDown(); 
-
+                log(chosen);
+                game.getLevel().move(player, chosen);
             }
+            log(chosen);
+            game.stop();
+        } catch (RuntimeException e) {
+            log(chosen);
+            game.stop();
         }
     }
 
-    /**
-     * Prints logs.
-     */
-    private static void log(Game game, Player player, Direction chosen, BufferedWriter writer) { 
-        try {
-            writer.write(String.format("%n%b %b %s %s %d %d", 
-                player.isAlive(), 
-                (player.getKiller() == null), 
-                player.getDirection(), 
-                chosen, 
-                game.getLevel().remainingPellets(), 
-                player.getScore()));
-        } catch (IOException e) { 
-        }
+    private Direction getRandomDirection() {
+        return Direction.values()[new Random().nextInt(Direction.values().length)];
+    }
+
+    private void log(Direction chosen) throws IOException {
+        Game game = launcher.getGame();
+        Player player = game.getPlayers().get(0);
+
+        logWriter.write(
+            String.format(
+                "%n%b %b %s %s %d %d",
+                player.isAlive(),
+                (player.getKiller() == null),
+                player.getDirection(),
+                chosen,
+                game.getLevel().remainingPellets(),
+                player.getScore()
+            )
+        );
     }
 }
